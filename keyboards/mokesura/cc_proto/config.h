f9eac3fb29a8062aa0b879c2447757e3b13213eb
千葉千夏 <47180063+mokesura@users.noreply.github.com>
2021-07-23 11:14:46 +0900

    Update config.h

--- keyboards/mokesura/cc_proto/config.h
+++ keyboards/mokesura/cc_proto/config.h
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID    0xFEED
+#define VENDOR_ID    0x1209
 #define PRODUCT_ID   0xCC00
 #define DEVICE_VER   0x0001
 #define MANUFACTURER Chinatsu Chiba (azulee/mokesura)
