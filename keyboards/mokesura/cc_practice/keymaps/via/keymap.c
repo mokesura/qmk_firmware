27dfd06fe37f5fa3e39fff1c0f5c8b1f9cf20bca
Chinatsu Chiba <47180063+mokesura@users.noreply.github.com>
2024-05-02 23:48:53 +0900

    for QMK Firmware 0.22.3

--- keyboards/mokesura/cc_practice/keymaps/via/keymap.c
+++ keyboards/mokesura/cc_practice/keymaps/via/keymap.c
@@ -2,57 +2,57 @@
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-    case USER00:
+    case QK_KB_0:
         if (record->event.pressed) {
-            SEND_STRING(SS_TAP(X_LANG2)"1D4\n");
+            SEND_STRING(SS_TAP(X_LNG2)"1D6\n");
         } else {
         }
         break;
-    case USER01:
+    case QK_KB_1:
         if (record->event.pressed) {
-            SEND_STRING(SS_TAP(X_LANG2)"1D6\n");
+            SEND_STRING(SS_TAP(X_LNG2)"1D6\n");
         } else {
         }
         break;
-    case USER02:
+    case QK_KB_2:
         if (record->event.pressed) {
-            SEND_STRING(SS_TAP(X_LANG2)"1D8\n");
+            SEND_STRING(SS_TAP(X_LNG2)"1D8\n");
         } else {
         }
         break;
-    case USER03:
+    case QK_KB_3:
         if (record->event.pressed) {
-            SEND_STRING(SS_TAP(X_LANG2)"1D10\n");
+            SEND_STRING(SS_TAP(X_LNG2)"1D10\n");
         } else {
         }
         break;
-    case USER04:
+    case QK_KB_4:
         if (record->event.pressed) {
-            SEND_STRING(SS_TAP(X_LANG2)"1D12\n");
+            SEND_STRING(SS_TAP(X_LNG2)"1D12\n");
         } else {
         }
         break;
-    case USER05:
+    case QK_KB_5:
         if (record->event.pressed) {
-            SEND_STRING(SS_TAP(X_LANG2)"1D20\n");
+            SEND_STRING(SS_TAP(X_LNG2)"1D20\n");
         } else {
         }
         break;
-    case USER06:
+    case QK_KB_6:
         if (record->event.pressed) {
-            SEND_STRING(SS_TAP(X_LANG2)"2D6\n");
+            SEND_STRING(SS_TAP(X_LNG2)"2D6\n");
         } else {
         }
         break;
-    case USER07:
+    case QK_KB_7:
         if (record->event.pressed) {
-            SEND_STRING(SS_TAP(X_LANG2)"3D6\n");
+            SEND_STRING(SS_TAP(X_LNG2)"3D6\n");
         } else {
         }
         break;
-    case USER08:
+    case QK_KB_8:
         if (record->event.pressed) {
-            SEND_STRING(SS_TAP(X_LANG2)"1D100\n");
+            SEND_STRING(SS_TAP(X_LNG2)"1D100\n");
         } else {
         }
         break;
@@ -69,19 +69,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───┴─────────┴───────┘
      */
     [0] = LAYOUT_ortho_2x3(
-        USER01, KC_MS_WH_UP,   LCTL(LSFT(KC_M)),
-        USER06, KC_MS_WH_DOWN, LCTL(LSFT(KC_D))
+        QK_KB_1, KC_MS_WH_UP,   LCTL(LSFT(KC_M)),
+        QK_KB_6, KC_MS_WH_DOWN, LCTL(LSFT(KC_D))
     ),
     [1] = LAYOUT_ortho_2x3(
-        _______, _______, _______,
-        _______, _______, _______
+        XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX
     ),
     [2] = LAYOUT_ortho_2x3(
-        _______, _______, _______,
-        _______, _______, _______
+        XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX
     ),
     [3] = LAYOUT_ortho_2x3(
-        _______, _______, _______,
-        _______, _______, _______
+        XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX
     )
 };
